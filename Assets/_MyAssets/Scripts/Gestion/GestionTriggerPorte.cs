db6eba364939c40b0c60c57c1f063acf8f832df5
antoineg94 <117280367+antoineg94@users.noreply.github.com>
2023-03-22 15:21:19 -0400

    fin niveau 1 debut niveau 2
    fin niveau 1
    ajout divers script
    debut niveau 2
    changemebt terrain

--- Assets/_MyAssets/Scripts/Gestion/GestionTriggerPorte.cs
+++ Assets/_MyAssets/Scripts/Gestion/GestionTriggerPorte.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class GestionTriggerPorte : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // variables
+    public GameObject ObjetDesactiver1;
+    public GameObject ObjetDesactiver2;
+    public GameObject ObjetActiver;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        ObjetDesactiver1.SetActive(false);//désactive la potion
+        ObjetDesactiver2.SetActive(false);//désactive égalemnt la fumée
+        ObjetActiver.transform.localEulerAngles = new Vector3(0, 0, 0);// tourne la porte d'un angle de 0 dégré ce qui va l'ouvrir
     }
 }
